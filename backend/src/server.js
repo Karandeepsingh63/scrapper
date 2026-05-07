f30dc36079f2b5f457065cc60bf87eea6c548f94
Karandeepsingh63 <deepsinghkaran12345@gmail.com>
2026-05-07 22:52:41 +0530

    Implement JWT

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,8 @@ const app=express();
 const User = require("./models/User");
 const Story = require("./models/Story");
 
+const authRoutes = require("./routes/authRoutes");
+
 // Connect to MongoDB
 connectDB();
 app.use(cors());
@@ -14,5 +16,7 @@ app.use(express.json());
 app.get("/",(req,res)=>{
     res.send("API is running...");
 }   )
+
+app.use("/api/auth", authRoutes);
 
 const PORT=process.env.PORT || 5000;                
