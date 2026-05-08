21d798c0b7213b2eda63ac3267cbe3d28442da4f
Karandeepsingh63 <deepsinghkaran12345@gmail.com>
2026-05-08 17:04:57 +0530

    fixed cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,10 +20,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(cors({
-  origin: "https://scrapper-1-31ej.onrender.com",
+  origin: "https://scrapper-su26.vercel.app",
   credentials: true
 }));
-
 
 
 app.use("/api/auth", authRoutes);
