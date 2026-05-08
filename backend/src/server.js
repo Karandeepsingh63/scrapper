864b58e86ed6d5ca22ba75e579fee47e4040ceb6
Karandeepsingh63 <deepsinghkaran12345@gmail.com>
2026-05-08 14:32:29 +0530

    fixed

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,47 @@
-const express=require('express');
-const connectDB=require('./config/db');
-const cors=require('cors');
-require('dotenv').config();
-const app=express();
-const User = require("./models/User");
-const Story = require("./models/Story");
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+const path = require("path");
+
 const authRoutes = require("./routes/authRoutes");
 const scrapeRoutes = require("./routes/scrapeRoutes");
-const scrapeStories = require("./services/scraperService");
 const storyRoutes = require("./routes/storyRoutes");
 
-// Connect to MongoDB
+const scrapeStories = require("./services/scraperService");
+
+const PORT = process.env.PORT || 5000;
+
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.use("/api/auth", authRoutes);
+app.use("/api/scrape", scrapeRoutes);
+app.use("/api/stories", storyRoutes);
+
+
+app.get("/api", (req, res) => {
+  res.send("API is running...");
+});
+
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+});
+
+
 const startServer = async () => {
   try {
     await connectDB();
+
     await scrapeStories();
 
     app.listen(PORT, () => {
@@ -26,23 +54,3 @@ const startServer = async () => {
 };
 
 startServer();
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/",(req,res)=>{
-    res.send("API is running...");
-}   )
-
-app.use("/api/auth", authRoutes);
-app.use("/api/scrape", scrapeRoutes);
-app.use("/api/stories", storyRoutes);
-
-app.use(express.static(path.join(__dirname, "dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
-
-
-const PORT=process.env.PORT || 5000;                
