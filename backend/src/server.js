cab2b7e6523a0a0e6ecf147de1327c9645932436
Karandeepsingh63 <deepsinghkaran12345@gmail.com>
2026-05-08 16:08:07 +0530

    API FIXING

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,6 +19,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(
+  cors({
+    origin: "https://scrapper-1-tdl7.onrender.com/api/stories",
+    credentials: true,
+  })
+);
 
 
 app.use("/api/auth", authRoutes);
