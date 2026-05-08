50cc8a3a1a174d9261a1d6d709ae8ced913df309
Karandeepsingh63 <deepsinghkaran12345@gmail.com>
2026-05-08 17:01:18 +0530

    API

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,6 +19,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(cors({
+  origin: "https://scrapper-1-31ej.onrender.com",
+  credentials: true
+}));
 
 
 
